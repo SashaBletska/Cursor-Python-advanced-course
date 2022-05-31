f968d75f728a612c390cd4abec15a4e609f4880a
Oleksandr Bletska <bletskasasha@gmail.com>
2022-05-31 15:02:31 +0300

    adding homework3 OOP

--- lecture3_oop/homework.py
+++ lecture3_oop/homework.py
@@ -44,13 +44,20 @@ class Human(Person, ABC):
 
 
 class House:
-    def __init__(self, house_name, house_area, house_cost):
+    def __init__(self, house_name, house_area, house_cost, house_discount):
         self.house_name = house_name
         self.house_area = house_area
         self.house_cost = house_cost
+        self.house_discount = house_discount
 
     def house_info(self):
         print(f'- House "{self.house_name}" with {self.house_area} square meters cost {self.house_cost}$.')
+
+    def discount_for_house(self):
+        self.house_cost = int(self.house_cost - self.house_cost * self.house_discount/100)
+        print(f'  For house "{self.house_name}" can be discount {self.house_discount}%.\n'
+              f'  Price with discount will be {self.house_cost}%!')
+
 
 
 class RealtorMeta(type):
@@ -64,15 +71,17 @@ class RealtorMeta(type):
 
 
 class Realtor(metaclass=RealtorMeta):
-    def __init__(self, name, discount_for_house):
+    def __init__(self, name, realtor_discount):
         self.name = name
-        self.discount_for_house = discount_for_house
+        self.realtor_discount = realtor_discount
 
     def info(self):
         print(f'Hello, my name is {self.name}, I will help you to find house of your dream:')
 
-    def discount(self):
-        print(f'You can get {self.discount_for_house}% discount.')
+    def discount(self, house):
+        house.house_cost = int(house.house_cost - house.house_cost * self.realtor_discount / 100)
+        print(f'-You can get plus my {self.realtor_discount}% discount:\n'
+              f'-Price for house {house.house_name} will be {house.house_cost}$!')
 
     def steal_money(self, human, house):
         if random.randint(1, 10) == 1 and human.availability_of_money >= house.house_cost:
@@ -87,25 +96,29 @@ realtor = Realtor('Ann', 10)
 
 realtor.info()
 
-house1 = House('Dream', 100, 55000)
-house2 = House('Sky', 150, 75000)
-house3 = House('Ocean', 200, 90000)
+house1 = House('Dream', 100, 55000, 5)
+house2 = House('Sky', 150, 75000, 7)
+house3 = House('Ocean', 200, 90000, 8)
 
 house1.house_info()
+house1.discount_for_house()
 house2.house_info()
+house2.discount_for_house()
 house3.house_info()
+house3.discount_for_house()
 
-realtor.discount()
 print('------------------------------------------------')
-tom = Human('Tom', 25, 50000, True)
+tom = Human('Tom', 25, 40000, True)
 tom.person_info()
 tom.person_make_money()
+realtor.discount(house1)
 tom.person_buy_a_house(house1)
 realtor.steal_money(tom, house1)
 print('------------------------------------------------')
-john = Human('John', 28, 70000, False)
+john = Human('John', 28, 52000, False)
 john.person_info()
 john.person_make_money()
+realtor.discount(house1)
 john.person_buy_a_house(house1)
 realtor.steal_money(john, house1)
 
@@ -113,6 +126,6 @@ print('------------------------------------------------')
 kate = Human('Kate', 30, 100000, True)
 kate.person_info()
 kate.person_make_money()
+realtor.discount(house3)
 kate.person_buy_a_house(house3)
 realtor.steal_money(kate, house3)
-
