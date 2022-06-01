85f96e26bb3523cc3e2c1689048e3280b21dcb45
Oleksandr Bletska <bletskasasha@gmail.com>
2022-06-01 11:29:21 +0300

    adding homework3 OOP

--- lecture3_oop/homework.py
+++ lecture3_oop/homework.py
@@ -54,10 +54,23 @@ class House:
         print(f'- House "{self.house_name}" with {self.house_area} square meters cost {self.house_cost}$.')
 
     def discount_for_house(self):
-        self.house_cost = int(self.house_cost - self.house_cost * self.house_discount/100)
+        self.house_cost = int(self.house_cost - self.house_cost * self.house_discount / 100)
         print(f'  For house "{self.house_name}" can be discount {self.house_discount}%.\n'
               f'  Price with discount will be {self.house_cost}%!')
 
+
+class SmallHouse(House):
+    def __init__(self, house_name, house_area, house_cost, house_discount):
+        super().__init__(house_name, house_area, house_cost, house_discount)
+
+    def small_house_info(self):
+        print(
+            f'-- There is also a small house "{self.house_name}" with a required area of {self.house_area} square meters')
+
+    def small_house_discount(self):
+        self.house_cost = int(self.house_cost - self.house_cost * self.house_discount / 100)
+        print(f'   Discount can be only {self.house_discount}%.\n'
+              f'   Price with discount {self.house_cost}$!')
 
 
 class RealtorMeta(type):
@@ -80,12 +93,14 @@ class Realtor(metaclass=RealtorMeta):
 
     def discount(self, house):
         house.house_cost = int(house.house_cost - house.house_cost * self.realtor_discount / 100)
-        print(f'-You can get plus my {self.realtor_discount}% discount:\n'
-              f'-Price for house {house.house_name} will be {house.house_cost}$!')
+        print(f'- You can get plus my {self.realtor_discount}% discount:\n'
+              f'- Price for house {house.house_name} will be {house.house_cost}$!')
 
     def steal_money(self, human, house):
         if random.randint(1, 10) == 1 and human.availability_of_money >= house.house_cost:
-            print(f'Realtor {self.name} had stolen {human.name} money!!!')
+            human.availability_of_money = 0
+            print(f'Realtor {self.name} had stolen {human.name}`s money!!!\n'
+                  f'Now {human.name} have {human.availability_of_money}$.')
         elif human.availability_of_money < house.house_cost:
             pass
         else:
@@ -99,6 +114,7 @@ realtor.info()
 house1 = House('Dream', 100, 55000, 5)
 house2 = House('Sky', 150, 75000, 7)
 house3 = House('Ocean', 200, 90000, 8)
+small_house = SmallHouse('Hunting Lodge', 40, 30000, 3)
 
 house1.house_info()
 house1.discount_for_house()
@@ -106,7 +122,8 @@ house2.house_info()
 house2.discount_for_house()
 house3.house_info()
 house3.discount_for_house()
-
+small_house.small_house_info()
+small_house.small_house_discount()
 print('------------------------------------------------')
 tom = Human('Tom', 25, 40000, True)
 tom.person_info()
@@ -129,3 +146,11 @@ kate.person_make_money()
 realtor.discount(house3)
 kate.person_buy_a_house(house3)
 realtor.steal_money(kate, house3)
+
+print('------------------------------------------------')
+Kim = Human('Kate', 22, 30000, False)
+Kim.person_info()
+Kim.person_make_money()
+realtor.discount(small_house)
+Kim.person_buy_a_house(small_house)
+realtor.steal_money(kate, small_house)
