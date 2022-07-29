ea9b088a4b5a3b506ae9f18150265561385d0de0
Oleksandr Bletska <bletskasasha@gmail.com>
2022-07-29 19:13:38 +0300

    resolved_task1

--- lecture16_regex/main.py
+++ lecture16_regex/main.py
@@ -2,17 +2,17 @@ import re
 
 print("=======================================================")
 print("1. Write a Python program which search a phone numbers.")
-pattern = r'[\d]{3}-[\d]{2}-[\d]{2}'
-text = 'Hello, my phone number is 125-98-70'
+pattern = r'\b[\d]{3}-?[\d]{2}-?[\d]{2}\b'
+text = 'Hello, my phone numbers is 125-98-70 and 8908934'
 res_phone = re.findall(pattern=pattern, string=text)
 
 
 def search_phone():
     if res_phone:
-        str_phone = ' '.join([str(elem) for elem in res_phone])
-        return f"Your phone number: {str_phone}"
+        str_phone = ', '.join([str(elem) for elem in res_phone])
+        return f"Your phone numbers: {str_phone}"
     else:
-        return "There is no phone number"
+        return "There is no phone numbers"
 
 
 print(search_phone())
@@ -20,7 +20,7 @@ print(search_phone())
 print("=========================================================================================================")
 print("2. Write a Python program basic validation for email. Local part should be consisted of lower/upper case,\n"
       "number, underscore and dot. Domain part - the same but dot symbol could not be the first character.")
-pattern = r'[A-Za-z0-9_\.]{1,255}@[^\.][A-Za-z0-9_\.]{1,255}'
+pattern = r'[A-Za-z0-9_\.]{1,255}@[^\.][A-Za-z0-9_\.]{1,255}\.[A-Za-z0-9_\.]{1,10}'
 text = 'cursor.test@cursor.com.ua'
 res_email = re.findall(pattern=pattern, string=text)
 
